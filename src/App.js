5c6941a6a54eb0167bb1df6e8c30bee2cd564fa8
Al <ismail3li@gmail.com>
2018-04-03 13:38:10 +0200

    passing state and mapping through it in a different component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,35 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-02T08:51:13+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-02T13:51:31+02:00
+ * @Last modified time: 2018-04-03T13:12:16+02:00
  */
 import React, { Component } from 'react';
 import './App.css';
 import Projects from './Components/Projects';
+
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
       projects: [
-        
+            {
+            title: 'eat meat',
+            category: 'Health and Fitness',
+            color: 'blue'
+          },
+          {
+            title: 'read Medium',
+            category: 'Coding skills',
+            color: 'red'
+          }
       ]
     }
   }
   render() {
     return (
       <div className="App">
-        My App
-      <Projects />
+      <Projects projects={this.state.projects}/>
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,15 +2,21 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-02T13:27:28+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-02T13:27:50+02:00
+ * @Last modified time: 2018-04-03T13:35:34+02:00
  */
  import React, { Component } from 'react';
-
  class Projects extends Component {
+
    render() {
+     let projectItems;
+     if(this.props.projects) {
+       projectItems = this.props.projects.map(project => {
+         console.log(project);
+       });
+     }
      return (
        <div className="Projects">
-         My Projects
+
        </div>
      );
    }
