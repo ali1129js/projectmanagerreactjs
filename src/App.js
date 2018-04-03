365a0aeb4710a6c0dea1ab6d02cccc1ef9e993f2
Al <ismail3li@gmail.com>
2018-04-03 14:22:04 +0200

    new component to add input forms

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-02T08:51:13+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-03T13:12:16+02:00
+ * @Last modified time: 2018-04-03T14:20:27+02:00
  */
 import React, { Component } from 'react';
 import './App.css';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
-            {
-            title: 'eat meat',
-            category: 'Health and Fitness',
-            color: 'blue'
-          },
-          {
-            title: 'read Medium',
-            category: 'Coding skills',
-            color: 'red'
-          }
-      ]
+      projects: []
     }
+  }
+  componentWillMount(){
+    this.setState({
+                projects:[
+                {
+                title: 'eat meat',
+                category: 'Health and Fitness',
+                color: 'blue'
+                  },
+                {
+                title: 'read Medium',
+                category: 'Coding skills',
+                color: 'red'
+                  }
+  ]});
   }
   render() {
     return (
       <div className="App">
-      <Projects projects={this.state.projects}/>
+        <AddProject />
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
---
+++ src/Components/AddProject.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-03T14:14:34+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-03T14:19:18+02:00
+ */
+import React, { Component } from 'react';
+class AddProject extends Component {
+  render(){
+    return(
+      <div>add project input placehodler</div>
+    );
+  }
+}
+export default AddProject;
