72e316f71a0d8d0501f335293d8483e01aa20916
Al <ismail3li@gmail.com>
2018-04-03 14:44:12 +0200

    outputing mapping categories as select options

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -2,13 +2,32 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-03T14:14:34+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-03T14:19:18+02:00
+ * @Last modified time: 2018-04-03T14:42:22+02:00
  */
 import React, { Component } from 'react';
 class AddProject extends Component {
+  static defaultProps = {
+    categories: ['Health and Fitness','Coding Skills','Knowledge Consumption','Entertainment']
+  }
   render(){
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key={category} value="category">{category}</option>
+    });
     return(
-      <div>add project input placehodler</div>
+      <div>
+        <form>
+          <div>
+            <label> Title</label><br />
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label> Category</label><br />
+            <select ref="Category">
+            {categoryOptions}
+          </select>
+          </div>
+        </form>
+      </div>
     );
   }
 }
